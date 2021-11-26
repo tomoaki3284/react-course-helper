031bf6b90bf5e2c9ea84b255eaea097de0a8fea2
Tomoaki Mitsuhashi <tomoaki3284@gmail.com>
2021-11-26 07:47:57 -0500

    Completed the skelton of courseSectionTable

--- src/components/explorePage/CourseSectionTable.js
+++ src/components/explorePage/CourseSectionTable.js
@@ -1,24 +1,84 @@
 import React from "react";
+import { useTable } from 'react-table'
 
-class CourseSectionTable extends React.Component {
-  constructor({courseSections}) {
-    super();
-    this.courseSections = courseSections;
-  }
+function CourseSectionTable({ courseSections }) {
+  console.log(courseSections)
+  const data = React.useMemo(
+    () => courseSections
+  )
 
-  render() {
-    return (
-      <div>
-        {
-          this.courseSections.map(function(item) {
-          return  <ul>
-                    <li key={item.courseCRN}>{item.courseCRN}</li>
-                  </ul>
-          })
-        }
-      </div> 
-    );
-  }
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'CRN',
+        accessor: 'courseCRN', // accessor is the "key" in the data
+      },
+      {
+        Header: 'Professor',
+        accessor: 'faculty',
+      },
+      {
+        Header: 'Time',
+        accessor: 'timeContent',
+      },
+      {
+        Header: 'Room',
+        accessor: 'room',
+      },
+      {
+        Header: 'Credit',
+        accessor: 'credit',
+      },
+    ],
+    []
+  )
+
+  return (
+    <div>
+      <Table columns={columns} data={data} />
+    </div>
+  )
+}
+
+function Table({ columns, data }) {
+  // Use the state and functions returned from useTable to build your UI
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({
+    columns,
+    data,
+  })
+
+  // Render the UI for your table
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row)
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+              })}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
 }
 
 export default CourseSectionTable;
