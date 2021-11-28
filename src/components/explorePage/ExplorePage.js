8f1a7608e357fed972cca9e05386f32836634551
Tomoaki Mitsuhashi <tomoaki3284@gmail.com>
2021-11-28 17:20:05 -0500

    migrate ExplorePage.js from class to hooks

--- src/components/explorePage/ExplorePage.js
+++ src/components/explorePage/ExplorePage.js
@@ -1,76 +1,55 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ManualScheduleTab from './manualScheduleTab';
 import FilterGroup from './FilterGroup';
 import '../../css/ExplorePage.css';
 import CourseFilter from '../../controller/CourseFilter';
 
-class ExplorePage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.courseFilter = new CourseFilter();
-    // use in course section table
-    this.handleAddCourse = props.handleAddCourse;
-    this.state = {
-      coursesMapByTitle: null,
-      filteredCourseMap: null,
-    };
-  }
+const ExplorePage = ({coursesMapByTitleProp, filteredCourseMapProp}) => {
+  const [coursesMapByTitle, setCoursesMapByTitle] = React.useState(coursesMapByTitleProp);
+  const [filteredCourseMap, setFilteredCourseMap] = React.useState(filteredCourseMapProp);
+  const courseFilter = new CourseFilter();
 
-  /**
-   * componentDidMount() is invoked immediately after a component is mounted
-   *  (inserted into the tree). 
-   * Initialization that requires DOM nodes should go here. 
-   * If you need to load data from a remote endpoint, 
-   * this is a good place to instantiate the network request.
-   */
-  async componentDidMount() {
-    // GET request using fetch with async/await
-    const courseMap = await this.fetchCourses();
-
-    console.log(courseMap);
-
-    this.setState({
-      coursesMapByTitle: courseMap,
-      filteredCourseMap: courseMap,
-    })
-  }
-
-  async fetchCourses() { 
-    const courses = await fetch('http://localhost:8081/api/v1/courses/group-by-title')
-    .then((response) => {
-      if (response.status >= 400 && response.status < 600) {
-        throw new Error("Bad response from server");
-      }
-      return response.json();
-    })
-    .catch((error) => {
-      console.log(error)
-    });
-
-    return courses;
-  }
-
-  onInputChangeInFilter(event, newValue, filterTitle) {
-    if (filterTitle === null || filterTitle === undefined) {
-      return;
+  useEffect(() => {
+    async function fetchCourses() {
+      // GET request using fetch with async/await
+      const courseMap = await fetchFromAPI();
+      setCoursesMapByTitle(courseMap);
+      setFilteredCourseMap(courseMap);
     }
 
-    this.courseFilter.setFilter(filterTitle, newValue);
-    const newFilteredCourseMap = this.courseFilter.filterCourse(this.state.coursesMapByTitle);
+    async function fetchFromAPI() { 
+      const courses = await fetch('http://localhost:8081/api/v1/courses/group-by-title')
+      .then((response) => {
+        if (response.status >= 400 && response.status < 600) {
+          throw new Error("Bad response from server");
+        }
+        return response.json();
+      })
+      .catch((error) => {
+        console.log(error)
+      });
+  
+      return courses;
+    }
+
+    fetchCourses();
+  });
+
+  function onInputChangeInFilter(event, newValue, filterTitle) {
+    if (filterTitle === null || filterTitle === undefined) return;
+
+    courseFilter.setFilter(filterTitle, newValue);
+    const newFilteredCourseMap = courseFilter.filterCourse(coursesMapByTitle);
     console.log(newFilteredCourseMap);
-    this.setState({
-      filteredCourseMap: newFilteredCourseMap,
-    });
+    setFilteredCourseMap(newFilteredCourseMap);
   }
 
-  render() {
-    return (
-      <div className='explore-container'>
-        <FilterGroup className='explore-container__sticky-filter'  onInputChange={this.onInputChangeInFilter.bind(this)}/>
-        <ManualScheduleTab coursesMapByTitle={this.state.filteredCourseMap} handleAddCourse={this.handleAddCourse}/>
-      </div>
-    );
-  }
+  return (
+    <div className='explore-container'>
+      <FilterGroup className='explore-container__sticky-filter'  onInputChange={onInputChangeInFilter}/>
+      <ManualScheduleTab coursesMapByTitle={filteredCourseMap}/>
+    </div>
+  );
 }
 
 export default ExplorePage
