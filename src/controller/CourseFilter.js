6630c194404e4aabb4b2c5ba84d15ed3dba575d1
Tomoaki Mitsuhashi <tomoaki3284@gmail.com>
2021-12-31 23:06:53 -0500

    added comment

--- src/controller/CourseFilter.js
+++ src/controller/CourseFilter.js
@@ -5,6 +5,17 @@ class CourseFilter {
     this.subject = '';
   }
 
+  /**
+   * Set the filter value on the certain filter
+   * 
+   * e.g.
+   * whichFilter = core
+   * newFilterValue = SOCU - social understanding
+   * In this case, it would set the value (SOCU) in the certain filter category (core)
+   * 
+   * @param {string} whichFilter - filter category that you want to modify
+   * @param {string} newFilterValue - value that would be filtered by
+   */
   setFilter(whichFilter, newFilterValue) {
     if (whichFilter === 'core') {
       this.core = newFilterValue;
@@ -28,15 +39,23 @@ class CourseFilter {
     return arr;
   }
 
+  /**
+   * Return filtered courses map by title after filtered based on the set current filter value
+   * 
+   * @param {Map<String,List<Course>>} courseMap - section courses map by title
+   * @return {Map<String,List<Course>>} Returns
+   */
   filterCourse(courseMap) {
     if (courseMap === undefined || courseMap === null) {
       return;
     }
 
+    // to make it easy to iterate and filter our class, convert map into array
     let courseMapArr = this.courseMapToArray(courseMap);
 
     const filteredCourseMap = new Map();
     
+    // special filter 'Double Core', because this needs to filter out course that fills two core 
     if (this.core === 'Double Core') {
       courseMapArr = courseMapArr.filter((courseEntry) => {
         const course = courseEntry.courses[0];
@@ -44,18 +63,22 @@ class CourseFilter {
       })
     }
 
+    // apply filter based on current set filter value
     courseMapArr.filter((courseEntry) => {
       const course = courseEntry.courses[0];
       return (this.emptyString(this.core) || course.cores.includes(this.core) || this.core === 'Double Core') &&
              (this.emptyString(this.mode) || course.room.toLocaleLowerCase() === this.mode) &&
              (this.emptyString(this.subject) || course.subject === this.subject);
     })
+    // then for each filtered course, add it to filteredCourseMap
     .forEach((filteredCourseEntry) => {
       const title = filteredCourseEntry.title;
       const courses = filteredCourseEntry.courses;
       filteredCourseMap.set(title, courses);
     })
   
+    // filteredCourseMap is typical Map structure, where each entry (courses map by title) is map by index.
+    // To return the Object of entries, which map courses to course title, it needs to be converted through Object::fromEntries
     return Object.fromEntries(filteredCourseMap);
   }
 
