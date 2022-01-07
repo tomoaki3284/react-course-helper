125983e890d61ae7e690f7790b1f8447368717be
Tomoaki Mitsuhashi <tomoaki3284@gmail.com>
2022-01-07 06:02:08 -0500

    animated open and close button

--- src/components/explorePage/CourseItem.js
+++ src/components/explorePage/CourseItem.js
@@ -4,14 +4,16 @@ import '../../css/courseItem.css';
 
 const CourseItem = ({course, courseSections, handleAddCourse}) => {
   const [expanded, setExpanded] = useState(false);
+  const [open, setOpen] = useState('close');
 
   const handleClick = () => {
     setExpanded(!expanded);
+    setOpen(open === 'open' ? 'close' : 'open');
   }
 
   return (
     <div className="course">
-      <p onClick={handleClick} className="course__title">{course.title}<span className='course__plus-icon'></span></p>
+      <p onClick={handleClick} className="course__title">{course.title}<span className={'course__plus-icon ' + open}></span></p>
       <p className="course__categories">{course.subject} | {course.credit}.0 | {course.cores.join(', ')}</p>
       <div className="course__description">
         {course.courseDescription}
